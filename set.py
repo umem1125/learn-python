2886784184d989bd8c760d67fb5ec09559a10afa
umem1125 <chaerulumam1125@gmail.com>
2026-01-01 19:18:10 +0700

    Modify in set.py

--- set.py
+++ set.py
@@ -1,11 +1,19 @@
 # set adalah kumpulan data yg tidak beraturan dan tidak memiliki elemen duplikat
 # set ditulis dengan kurung kurawal {} atau dengan fungsi bawaan set()
 
-angka =  {1}
+# angka =  {1}
 
-angka.add(2)
-angka.add(3)
-angka.add(100)
+# angka.add(2)
+# angka.add(3)
+# angka.add(100)
 
-for i in angka:
+buah = {"jeruk", "apel", "pisang"}
+print(buah)
+
+buah.add("mangga")
+print(buah)
+buah.remove("jeruk")
+print(buah)
+
+for i in buah:
     print(i)
